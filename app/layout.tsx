81f367a8e61e3cc94ff61ea567adc005442d008e
Hassan Najem <hassanmosul@hotmail.com>
2025-10-02 20:45:11 +0300

    sdsdsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,9 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover', // Handles iOS notch/safe areas
   themeColor: '#070707',
 };
 
