b9d33790ed3841c24f25c8caebee0a5425fafccb
Hassan Najem <hassanmosul@hotmail.com>
2026-03-13 21:28:25 +0100

    Update login and signup pages to use new design

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative min-h-screen grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       {/* Theme Toggle - Top Right */}
       <div className="absolute top-4 right-4 z-30">
         <ThemeToggleButton start="top-right" />
@@ -34,8 +34,8 @@ export default function LoginPage() {
           </blockquote>
         </div>
       </div>
-      <div className="py-8 lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="overflow-y-auto py-8 lg:p-8">
+        <div className="mx-auto flex w-full flex-col space-y-6 sm:w-[350px] my-auto">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight text-black dark:text-white">
               Welcome back
